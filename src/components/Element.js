6d348d3d4afcadb33a71b6a48de8a1c9f4463a20
Ksinia <xiatian@mail.ru>
2019-11-18 16:42:29 +0100

    Structure of basic element

---
+++ src/components/Element.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Element extends Component {
+  render() {
+    return (
+      <div className="element">
+        <h4 className="date">{this.props.date}</h4>
+        <h3 className="title">{this.props.title}</h3>
+        <img className="picture" src={this.props.imgsrc}></img>
+        <p className="description">{this.props.description}</p>
+        <div className="likes">Likes will be here someday</div>
+      </div>
+    );
+  }
+}
